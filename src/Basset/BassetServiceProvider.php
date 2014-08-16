f245b58c1feb3994d3424010b4ce59dbe04a3538
Martin Mårtensson <Marwelln@gmail.com>
2014-08-16 13:20:18 +0200

    Updated package name

--- src/Basset/BassetServiceProvider.php
+++ src/Basset/BassetServiceProvider.php
@@ -50,7 +50,7 @@ class BassetServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('jasonlewis/basset', 'basset', __DIR__.'/../');
+        $this->package('marwelln/basset', 'basset', __DIR__.'/../');
 
         // Tell the logger to use a rotating files setup to log problems encountered during
         // Bassets operation but only when debugging is enabled.
